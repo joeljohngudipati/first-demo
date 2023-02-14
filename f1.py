dc7865ef2f463cbe5296c2179e25d6d70a32aa3d
joeljohngudipati <joeljogudipati@gmail.com>
2023-02-14 19:14:31 +0530

    joo

--- f1.py
+++ f1.py
@@ -16,17 +16,66 @@
 #     print("name:{}\n age:{}\n salary;{}".format(name,age,salary))
 # personal_details()
 
-def getdata():
-    print("Enter your details\n")
-    list1=[]
-    for var in range(3):
-        name = input("Enter your name: ") # string
-        age = int(input("Enter your age: "))  # integer
-        salary = float(input("Enter your salary: ")) # float
-        list1.append([name,age,salary])
-    print(list1)
-    display(list1)
-def display(LIST1):
-    for i in range(3):
-        print("Hello ",LIST1[i][0]," as your age is ",LIST1[i][1]," and your salary is ",LIST1[i][2]," so we offer you the loan.")
-getdata()
+# def getdata():
+#     print("Enter your details\n")
+#     list1=[]
+#     for var in range(3):
+#         name = input("Enter your name: ") # string
+#         age = int(input("Enter your age: "))  # integer
+#         salary = float(input("Enter your salary: ")) # float
+#         list1.append([name,age,salary])
+#     print(list1)
+#     display(list1)
+# def display (LIST1):
+#     for i in range(3):
+#         print("Hello ",LIST1[i][0]," as your age is ",LIST1[i][1]," and your salary is ",LIST1[i][2]," so we offer you the loan.")
+# getdata()
+
+
+# ?
+
+# Enter your name: jolrl
+# Enter your age: 23
+# Enter your salary: 53535
+# Choose your option:
+# 1.Insert
+# 2.Remove
+# 3.Display
+# 4.Exit
+
+# Choose an option: 2
+# jolrl
+# 23
+# 53535.0
+# Type the element to remove:23
+# Traceback (most recent call last):
+#   File "c:\Users\joelj\OneDrive\Desktop\joeljo\DEMO1", line 56, in <module>
+#     removeelements()
+#   File "c:\Users\joelj\OneDrive\Desktop\joeljo\DEMO1", line 44, in removeelements
+#     list1.remove(element)
+# ValueError: list.remove(x): x not in list
+# PS C:\Users\joelj\OneDrive\Desktop\joeljo>joel
+
+
+# a,b,c=2,3,4
+# print(a+b+c)
+
+# num=54
+# print(num)
+
+n=33
+if(n%2==0):
+    print("even"),n
+else:  
+     print("odd"),n
+
+
+# a,b,c=3,5,7
+
+
+# print(a+c)
+
+
+# a,b,c=2,4,6
+# print(a*b+c)
+
