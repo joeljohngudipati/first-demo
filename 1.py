c5172acafbfc77ac42df8cec5f2d56392c0d5a33
joeljohngudipati <joeljogudipati@gmail.com>
2023-02-16 18:16:34 +0530

    16-2-23

---
+++ 1.py
@@ -0,0 +1,71 @@
+# a,b=6,7
+# print(a+b)
+
+# a=2
+# sqrt=a**0.5
+# print(sqrt)
+
+# base=4
+# height=5
+# print(0.5*base*height)
+
+
+# p,t,r=3000,3,5
+# si=(p*t*r)/100
+# print(si)   
+
+# r=4
+# pi=22/7
+# area=pi*r*r
+
+# a=4
+# print(a*a)
+
+# n=56
+# if(n%2==0):
+#     print("even"),n
+# else:  
+#      print("odd"),n
+
+
+# p,t,r=3000,4,10
+# a=p*(1+(r/100))**t
+# ci=a-p
+# print(ci)
+
+
+# # Program to display the Fibonacci sequence up to n-th term
+# nterms = int(input("How many terms? "))
+# # first two terms
+# n1, n2 = 0, 1
+# count = 0
+# # check if the number of terms is valid
+# if nterms <= 0:
+#    print("Please enter a positive integer")
+# # if there is only one term, return n1
+# elif nterms == 1:
+#    print("Fibonacci sequence upto",nterms,":")
+#    print(n1)
+# # generate fibonacci sequence
+# else:
+#    print("Fibonacci sequence:")
+#    while count < nterms:
+#        print(n1)
+#        nth = n1 + n2
+#        # update values
+#        n1 = n2
+#        n2 = nth
+#        count += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
